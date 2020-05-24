f72d31229bbc4210535baf683f79c10cc37cb7d5
HoJin <dudu016@naver.com>
2020-05-25 00:31:33 +0900

    속도 감소

--- programmers/stack_queue/P42583.java
+++ programmers/stack_queue/P42583.java
@@ -1,5 +1,27 @@
 import java.util.*;
+class Solution {
+        public int solution(int bridge_length, int weight, int[] truck_weights) {
+            Queue<Integer> bridge = new LinkedList<>();
+            int time = 0;
+            int nWeight = 0;
+            for (int truck : truck_weights) {
+                while (true){
+                    time++;
+                    if(bridge.size() == bridge_length) nWeight-=bridge.poll();
+                    if(nWeight+truck<=weight){
+                        bridge.add(truck);
+                        nWeight+=truck;
+                        break;
+                    }else{
+                        bridge.add(0);
+                    }
+                }
 
+            }
+            return time+bridge_length;
+        }
+}
+=======================================================================================================
 class Solution {
         public int solution(int bridge_length, int weight, int[] truck_weights) {
             Queue<Integer> bridge = new LinkedList<>();
