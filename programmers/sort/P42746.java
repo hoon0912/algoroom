cdeaac3fb74e701f7f3aa6432dca35aa35f42cb3
HoJin <dudu016@naver.com>
2020-06-14 18:53:45 +0900

    Update P42746.java

--- programmers/sort/P42746.java
+++ programmers/sort/P42746.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 class Solution {
     public String solution(int[] numbers) {
-        if(Arrays.stream(numbers).filter(x->x > 0).toArray().length == 0) return "0";
-        return Arrays.stream(numbers).mapToObj(String::valueOf).sorted((o1, o2) -> -o1.concat(o2).compareTo(o2.concat(o1))).collect(Collectors.joining());
+        return (Arrays.stream(numbers).filter(x->x > 0).toArray().length == 0) ? "0" : Arrays.stream(numbers).mapToObj(String::valueOf).sorted((o1, o2) -> -o1.concat(o2).compareTo(o2.concat(o1))).collect(Collectors.joining());
     }
 }
