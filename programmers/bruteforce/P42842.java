56925810c0471ddcb4438766c599cd9681aea67f
HoJin <dudu016@naver.com>
2020-06-21 22:58:53 +0900

    Update P42842.java

--- programmers/bruteforce/P42842.java
+++ programmers/bruteforce/P42842.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        for(int i = 1,carpetSize = brown+yellow; i<= carpetSize; i++)
-            for (int j = i; j <= carpetSize; j++)
+        for(int i = 2,carpetSize = brown+yellow; i<= carpetSize/2; i++)
+            for (int j = i; j <= carpetSize/2; j++)
                 if (i * 2 + (j - 2) * 2 == brown && (i - 2) * (j - 2) == yellow)
                     return new int[]{Math.max(i,j), Math.min(i,j)};
         return null;
