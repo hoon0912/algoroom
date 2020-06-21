91e13a9cd7a30898e942e78c20454653f0811d7f
HoJin <dudu016@naver.com>
2020-06-21 22:52:24 +0900

    Update P42840.java

--- programmers/bruteforce/P42840.java
+++ programmers/bruteforce/P42840.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] checkCount = Arrays.stream(new int[][]{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}}).mapToInt(x->IntStream.range(0, answers.length).map(y->answers[y] == x[y%x.length] ? 1: 0).sum()).toArray();
+        int[] checkCount = Arrays.stream(new int[][]{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}})
+            .mapToInt(x->IntStream.range(0, answers.length).map(y->answers[y] == x[y%x.length] ? 1: 0).sum()).toArray();
         int maxCount = Arrays.stream(checkCount).max().getAsInt();
         return IntStream.range(1,4).filter(x->checkCount[x-1] == maxCount).toArray();
     }
